711221469e915fb1108a13b4975c3d23e06523a6
Narvaez2001 <ncnrvz2001@gmail.com>
2024-08-12 21:34:24 +0200

    update carrusel de casas

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 // Owlcarousel
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-  	loop:true,
-    margin:10,
-    nav:true,
-	autoplay:true,
-    autoplayTimeout:9000,
-    autoplayHoverPause:true,
-    center: true,
-    navText: [
-	    "<i class='fa fa-angle-left'></i>",
-	    "<i class='fa fa-angle-right'></i>"
-	],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
-        }
-    }
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+      margin: 10,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 9000,
+      autoplayHoverPause: true,
+      center: true,
+      navText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+      ],
+      responsive:{
+          0:{
+              items: 1,
+              nav: false // Desactiva las flechas en pantallas pequeñas
+          },
+          600:{
+              items: 1,
+              nav: false // Desactiva las flechas en pantallas medianas
+          },
+          1000:{
+              items: 3,
+              nav: true // Activa las flechas en pantallas grandes
+          }
+      }
+    });
   });
-});
+  
